e1a8b0c734f12172e5140355082c07833518fa51
Harvey Deng <Harvey.Deng@morningstar.com>
2019-02-18 14:50:16 +0800

    update Jenkinsfile and install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,4 @@ export JAVA_HOME=/usr/java/jdk1.8.0_92
 export PATH=$JAVA_HOME/bin:$PATH
 export CLASSPATH=.:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/jre 
 
-
 java -jar com.deng-1.0-SNAPSHOT.jar >/data/console.txt 2>&1 &
-
-
-
