d9bfb4bcc5bebc6b0303245fb635102b8938a27b
Harvey Deng <Harvey.Deng@morningstar.com>
2019-02-18 10:31:07 +0800

    commit

---
+++ src/test/java/helloworld/AppTest.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Test Hello World!");
+        assertTrue( true );
+    }
+}
